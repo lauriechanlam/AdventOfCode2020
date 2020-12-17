cf0bdce89a4232a86d9e68610aefebed0c69ff61
Laurie Chan Lam <laurie@lady-pony.com>
2020-12-18 00:33:32 +0100

    day 17 - works slow

--- challenge-2020-17.py
+++ challenge-2020-17.py
@@ -1,169 +1,79 @@
-import itertools
-import re
-import numpy
 import utils
 import copy
+import numpy
+import itertools
 
 
 def read_input(filename):
-    text = utils.read(filename, 'string').splitlines()
+    lines = utils.read(filename, 'string').splitlines()
+    return numpy.array([[char == '#' for char in line] for line in lines])
 
-    #pattern = re.compile(r'(?P<start>\d+)-(?P<end>\d+)')
 
-    arr = [[False, False, False, False, False, False, False] +  [c == '#' for c in line] + [False, False, False, False, False, False, False]  for line in text]
+def pad(grid):
+    return numpy.pad(grid, (1, 1), 'constant', constant_values=(False, False))
 
-    for i in range(0, 7):
-        arr.insert(0, [False for i in range(0, len(arr[0]))])
-        arr.append([False for i in range(0, len(arr[0]))])
-    return arr
 
 class Part1(utils.Part):
     def __init__(self):
-        super().__init__(None)
+        super().__init__(112)
+
+    @staticmethod
+    def get_state(cells):
+        count = numpy.sum(cells)
+        if cells[1][1][1]:
+            return 2 < count < 5
+        return count == 3
+
+    @staticmethod
+    def cycle(grid):
+        grid = pad(grid)
+        new_grid = copy.deepcopy(grid)
+
+        for x in range(1, len(new_grid) - 1):
+            for y in range(1, len(new_grid[x]) - 1):
+                for z in range(1, len(new_grid[x][y]) - 1):
+                    cells = grid[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2]
+                    new_grid[x][y][z] = Part1.get_state(cells)
+
+        return new_grid
 
     def run(self, input, is_test):
-        act = {
-            True: lambda x: True if 2 <= sum(x) <= 3 else False,
-            False: lambda x: True if sum(x) == 3 else False
-        }
+        grid = pad(numpy.array([input]))
 
-        d = [[[False for i in inp] for inp in input],
-            [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-            [[False for i in inp] for inp in input],
-            [[False for i in inp] for inp in input],
-            [[False for i in inp] for inp in input],
-            input,
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input],
-             [[False for i in inp] for inp in input]]
-        #print_arr(d)
         for round in range(0, 6):
-            d2= copy.deepcopy(d)
-            for i, x in enumerate(d):
-                if i == 0 or i == len(d)-1:
-                    continue
+            grid = Part1.cycle(grid)
 
-                for j, y in enumerate(x):
-                    if j == 0 or j == len(x) - 1:
-                        continue
-                    for k, z in enumerate(y):
-                        if k == 0 or k == len(y) - 1:
-                            continue
-                        se = d[i][j][k]
-                        ne = []
-                        for i2 in range(i-1, i+2):
-                            for j2 in range(j-1, j+2):
-                                for k2 in range(k-1, k+2):
-                                    if i2 == i and j2 == j and k2 == k:
-                                         continue
-                                    ne.append(d[i2][j2][k2])
-                        d2[i][j][k] = act[se](ne)
-            #print('============Round {}'.format(round))
-            #print_arr(d2)
-            d = d2
+        return numpy.sum(grid)
 
-        v = 0
-        for i2 in d:
-            for j2 in i2:
-                for k2 in j2:
-                    v += k2
-        return v
 
-def make_3(d):
-
-    r = []
-    for i in range(0, 7):
-        add = [[False for i in range(0, len(d[j]))] for j in range(0, len(d))]
-        r.append(add)
-    r.append(d)
-    for i in range(0, 7):
-        add = [[False for i in range(0, len(d[j]))] for j in range(0, len(d))]
-        r.append(add)
-    return r
-
-def make_4(d):
-    r = []
-    for i in range(0, 7):
-        add = [[[False for k in j] for j in i] for i in d]
-        r.append(add)
-    r.append(d)
-    for i in range(0, 7):
-        add = [[[False for k in j] for j in i] for i in d]
-        r.append(add)
-    return r
-
-class Part1(utils.Part):
+class Part2(utils.Part):
     def __init__(self):
         super().__init__(848)
 
+    @staticmethod
+    def get_state(cells):
+        count = numpy.sum(cells)
+        if cells[1][1][1][1]:
+            return 2 < count < 5
+        return count == 3
+
+    @staticmethod
+    def cycle(grid):
+        grid = pad(grid)
+        new_grid = copy.deepcopy(grid)
+
+        for x in range(1, len(new_grid) - 1):
+            for y in range(1, len(new_grid[x]) - 1):
+                for z in range(1, len(new_grid[x][y]) - 1):
+                    for w in range(1, len(new_grid[x][y][z]) - 1):
+                        cells = grid[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2, w - 1:w + 2]
+                        new_grid[x][y][z][w] = Part2.get_state(cells)
+        return new_grid
+
     def run(self, input, is_test):
-        d = make_4(make_3(input))
-        act = {
-            True: lambda x: True if 2 <= sum(x) <= 3 else False,
-            False: lambda x: True if sum(x) == 3 else False
-        }
+        grid = pad(numpy.array([numpy.array([input])]))
 
         for round in range(0, 6):
-            print('============Round {}'.format(round))
-            d2= copy.deepcopy(d)
-            for i, x in enumerate(d):
-                print(i)
-                if i == 0 or i == len(d)-1:
-                    continue
+            grid = Part2.cycle(grid)
 
-                for j, y in enumerate(x):
-                    if j == 0 or j == len(x) - 1:
-                        continue
-                    for k, z in enumerate(y):
-                        if k == 0 or k == len(y) - 1:
-                            continue
-                        for l, w in enumerate(z):
-                            if l == 0 or l == len(z) - 1:
-                                continue
-                            se = d[i][j][k][l]
-                            ne = []
-                            for i2 in range(i-1, i+2):
-                                for j2 in range(j-1, j+2):
-                                    for k2 in range(k-1, k+2):
-                                        for l2 in range(l - 1, l + 2):
-                                            if i2 == i and j2 == j and k2 == k and l == l2:
-                                                continue
-                                            ne.append(d[i2][j2][k2][l2])
-                            d2[i][j][k][l] = act[se](ne)
-            #
-            #print_arr4(d2)
-            d = d2
-
-        v = 0
-        for i2 in d:
-            for j2 in i2:
-                for k2 in j2:
-                    for l2 in k2:
-                        v += l2
-        #if v > 976:
-        return v
-
-
-
-def print_arr(r):
-    s = [[''.join(['#' if w else '.' for w in y]) for y in x] for x in r]
-    s =['\n'.join(x) for x in s]
-    for x in s:
-        print(x)
-        print('')
-
-def print_arr4(r):
-    s = [[[''.join(['#' if w else '.' for w in y]) for y in x] for x in w] for w in r]
-    s = [['\n'.join(x) for x in d] for d in s]
-    for i, d in enumerate(s):
-        for j, k in enumerate(d):
-            print('w={} z={}'.format(i, j))
-            print(k)
-            print('')
-    print('')
+        return numpy.sum(grid)
