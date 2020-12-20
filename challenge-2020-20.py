f2d84a146bafdd8c2611d4b06bc1654a7cd34864
Laurie Chan Lam <laurie@lady-pony.com>
2020-12-20 13:32:57 +0100

    day 20 part 2 - dirty

--- challenge-2020-20.py
+++ challenge-2020-20.py
@@ -10,12 +10,12 @@ import copy
 tile_pattern = re.compile(r'Tile (?P<ID>\d+):')
 
 def read_input(filename):
+    return utils.read(filename, 'string')
     tiles = utils.read(filename, 'string').split('\n\n')
     def make_tile(lines):
         id = int(tile_pattern.fullmatch(lines[0]).group('ID'))
         lines = lines[1:]
-        borders = [lines[0], ''.join([line[-1] for line in lines]), lines[-1][::-1], ''.join([line[0] for line in lines][::-1])]
-        return id, borders
+        return id, lines
     tiles = [make_tile(tile.splitlines()) for tile in tiles]
     return {tile[0]: tile[1] for tile in tiles}
 #
@@ -56,6 +56,8 @@ def read_input(filename):
 # def corners(combi):
 #     n = int(math.sqrt(len(combi)))
 #     return [combi[0][0], combi[0][-1], combi[-1][0], combi[-1][-1]]
+
+HASH = '0'
 
 
 class Stack:
@@ -105,8 +107,44 @@ class Stack:
             print(self)
             print(self.corner_ids())
             print(math.prod(self.corner_ids()))
+            self.write_image('resources/2020-20-image.txt')
             exit(0)
         return True
+
+    def write_image(self, filename):
+        def make_image(id, borders, rotation, flip):
+            pixels = Part1.tiles[id]
+            pixels = [line[1:-1] for line in pixels[1:-1]]
+            if flip:
+                pixels = [''.join(line) for line in numpy.transpose([[c for c in line] for line in pixels])]
+            for _ in range(0, rotation):
+                pixels = [''.join(line) for line in numpy.rot90([[c for c in line] for line in pixels])]
+            return pixels
+
+        tile_images = []
+        for i in range(0, self.n):
+            tile_row = []
+            for j in range(0, self.n):
+                tile_row.append(make_image(*self.tiles[self.n * i + j]))
+            tile_images.append(tile_row)
+
+        s = []
+        for tile_row in tile_images:
+            for k in range(0, 8)[::-1]:
+                line = ''.join([''.join(tile[k]) for tile in tile_row])
+                s.append(line)
+        t = [[c for c in line] for line in s]
+        t = '\n'.join([''.join(line) for line in t])
+
+
+
+        with open(filename, 'w') as f:
+            f.write(t)
+
+
+        #print(tile_images)
+
+
 
     def transform_tile(self, idx):
         (id, borders, rotation, flip) = self.tiles[idx]
@@ -135,18 +173,78 @@ def try_add_tiles(stack, tiles):
             if stack_size == len(stack.tiles):
                 stack.pop()
 
+pattern_all = re.compile(r'\n([\.\#]*)\#[\.\#][\.\#][\.\#][\.\#]\#\#[\.\#][\.\#][\.\#][\.\#]\#\#[\.\#][\.\#][\.\#][\.\#]\#\#\#([\.\#]*)\n([\.\#]*)[\.\#]\#[\.\#][\.\#]\#[\.\#][\.\#]\#[\.\#][\.\#]\#[\.\#][\.\#]\#[\.\#][\.\#]\#[\.\#][\.\#][\.\#]([\.\#]*)')
+
+
+def count_monsters(txt):
+    monster_count = 0
+    for maybe in pattern_all.finditer(txt):
+        offset = set(len(g) for g in maybe.groups()[::2])
+        if len(offset) != 1:
+            continue
+        offset = offset.pop()
+        first_line = txt[:maybe.start()].splitlines()[-1]
+        if first_line[offset + 18] == HASH:
+            monster_count += 1
+    return monster_count
+
+
+##pattern_no_left = re.compile(r'[\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#][\.\#]#\n#[\.\#][\.\#][\.\#][\.\#]##[\.\#][\.\#][\.\#][\.\#]##[\.\#][\.\#][\.\#][\.\#]###\n[\.\#]#[\.\#][\.\#]#[\.\#][\.\#]#[\.\#][\.\#]#[\.\#][\.\#]#[\.\#][\.\#]#[\.\#][\.\#][\.\#]')
+pattern_no_left = re.compile(r'[X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0][X0]0[X0]\n0[X0][X0][X0][X0]00[X0][X0][X0][X0]00[X0][X0][X0][X0]000\n[X0]0[X0][X0]0[X0][X0]0[X0][X0]0[X0][X0]0[X0][X0]0[X0][X0][X0]')
+
+
+def count_monsters(txt):
+    monster_count = 0
+    lines = txt.splitlines()
+    for i in range(0, len(lines[0]) - 20):
+        r = '\n'.join([line[i:i+20] for line in lines])
+        for maybe in pattern_no_left.finditer(r):
+            monster_count += 1
+    return monster_count
+
+class Part2(utils.Part):
+    def __init__(self):
+        super().__init__(273)
+
+    def run(self, txt, is_test):
+        with open('resources/test.txt' if is_test else 'resources/-2.txt') as f:
+            txt = f.read()
+
+        total = sum(x == HASH for x in txt)
+
+        for flip in False, True:
+            v = [[c for c in line] for line in txt.splitlines()]
+            if flip:
+                v = numpy.transpose(v)
+            for rotation in range(0, 4):
+                v = numpy.rot90(v)
+                t = '\n'.join([''.join(line) for line in v])
+                print(flip, rotation)
+                monster_count = count_monsters(t)
+                if monster_count != 0:
+                    r = total - monster_count * 15
+                    if r in [1987, 2077, 2287]:
+                        print(total)
+                        print(r)
+                        print('Tooh')
+                        exit(1)
+                    return r
+
 class Part1(utils.Part):
     def __init__(self):
         super().__init__(None)#20899048083289)
 
     def run(self, tiles, is_test):
+        Part1.tiles = copy.deepcopy(tiles)
 
         stack = Stack(int(math.sqrt(len(tiles))))
 
         transformed_tiles = []
-        for id, borders in tiles.items():
+        for id, lines in tiles.items():
+            borders = [lines[0], ''.join([line[-1] for line in lines]), lines[-1][::-1], ''.join([line[0] for line in lines][::-1])]
+            tiles[id] = borders
             for rotation in range(0, 4):
-                for flip in True, False:
+                for flip in False, True:
                     transformed_tiles.append((id, borders, rotation, flip))
 
         try_add_tiles(stack, transformed_tiles)
