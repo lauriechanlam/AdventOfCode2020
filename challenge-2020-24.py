06b2b16a71693ce76808b847f5105eab7e573e1f
Laurie Chan Lam <laurie@lady-pony.com>
2020-12-24 11:36:31 +0100

    day 24 - clean

--- challenge-2020-24.py
+++ challenge-2020-24.py
@@ -1,9 +1,9 @@
 import utils
-from collections import defaultdict
 import re
 
 
 pattern = re.compile(r'(e|se|sw|w|nw|ne)')
+
 
 def parse_line(line):
     return pattern.finditer(line)
@@ -13,23 +13,30 @@ def read_input(filename):
     text = utils.read(filename, 'string')
     return text.splitlines()
 
-mp = {
-        'e': (2, 0),
-        'w': (-2, 0),
-        'ne': (1, 2),
-        'nw': (-1, 2),
-        'se': (1, -2),
-        'sw': (-1, -2),
-    }
+
+directions = {
+    'e': (2, 0),
+    'w': (-2, 0),
+    'ne': (1, 1),
+    'nw': (-1, 1),
+    'se': (1, -1),
+    'sw': (-1, -1),
+}
+
 
 def position(line):
-    pos = (0, 0)
-    for move in pattern.finditer(line):
-        #print(move.groups()[0])
-        m = mp[move.groups()[0]]
-        pos = (pos[0] + m[0], pos[1] + m[1])
-        #print(pos)
-    return pos
+    moves = [directions[move.groups()[0]] for move in pattern.finditer(line)]
+    return tuple(sum(x) for x in zip(*moves))
+
+
+def get_black(positions):
+    black = set()
+    for p in positions:
+        if p in black:
+            black.remove(p)
+        else:
+            black.add(p)
+    return black
 
 
 class Part1(utils.Part):
@@ -38,43 +45,39 @@ class Part1(utils.Part):
         super().__init__(10)
 
     def run(self, tiles, is_test):
-        pos = [position(tile) for tile in tiles]
-        d = defaultdict(int)
-        for p in pos:
-            d[p] += 1
+        positions = [position(tile) for tile in tiles]
+        black = get_black(positions)
+        return len(black)
 
-        s = 0
-        for p in d.values():
-            if p%2 == 1:
-                s += 1
-        return s
 
 def make_bool():
     return False
 
+
 def make_padding(tiles):
     for tile in tiles:
-        [(tile[0] + m[0], tile[1] + m[1]) for m in mp.values()]
+        [(tile[0] + m[0], tile[1] + m[1]) for m in directions.values()]
 
-def flip_tiles(inp):
+
+def get_neighbors(tile):
+    return set([tuple(sum(x) for x in zip(tile, direction)) for direction in directions.values()])
+
+
+def flip_tiles(black):
+
     tiles = set()
-    for tile in inp:
-        neighbors = [(tile[0]+m[0], tile[1]+m[1]) for m in mp.values()]
-        n = len(inp.intersection(neighbors))
+    for tile in black:
+        neighbors = get_neighbors(tile)
+        n = len(neighbors.intersection(black))
         if 0 < n <= 2:
             tiles.add(tile)
         for t in neighbors:
-            nbg = [(t[0] + m[0], t[1] + m[1]) for m in mp.values()]
-            n = len(inp.intersection(nbg))
-            if t not in inp:  # white
-                if n == 2:
-                    tiles.add(t)
-            else:
-                if 0 < n <= 2:
-                    tiles.add(t)
-
+            if t in black:
+                continue
+            n = len(get_neighbors(t).intersection(black))
+            if n == 2:
+                tiles.add(t)
     return tiles
-
 
 
 class Part2(utils.Part):
@@ -83,21 +86,10 @@ class Part2(utils.Part):
         super().__init__(2208)
 
     def run(self, tiles, is_test):
-        pos = [position(tile) for tile in tiles]
-        d = defaultdict(int)
-        for p in pos:
-            d[p] += 1
-
-        is_black = set()
-        for tile, p in d.items():
-            if p%2 == 1:
-                is_black.add(tile)
-
-        print('day {}: {}'.format(0, len(is_black)))
+        positions = [position(tile) for tile in tiles]
+        black = get_black(positions)
 
         for i in range(0, 100):
-            is_black = flip_tiles(is_black)
-            print('day {}: {}'.format(i+1, len(is_black)))
+            black = flip_tiles(black)
 
-
-        return len(is_black)
+        return len(black)
